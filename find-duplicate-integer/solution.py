83ea7f100ce45d49337006490aaa2824c0692947
Ellis <minibroel@gmail.com>
2026-02-08 17:18:53 +0000

    Binary search

--- find-duplicate-integer/solution.py
+++ find-duplicate-integer/solution.py
@@ -1,8 +1,17 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # we can use a set "found" this is O(n) space and time
-        found = set()
-        for num in nums:
-            if num in found:
-                return num
-            found.add(num)
+        # binary search the numbers not the indices
+        l = 1 
+        r = len(nums) - 1
+        # use the fact that at mid, to the left there should be <= mid elements there.
+        while l<r:
+            mid = (r+l) //2
+            count = sum(1 for num in nums if num <= mid)
+
+            if count > mid:
+                r = mid 
+            else:
+                l = mid + 1
+            
+        return l
+        
