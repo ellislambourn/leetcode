d33997c1fb99351046c54520a1cadf5e271b7c48
Ellis <minibroel@gmail.com>
2026-02-09 12:57:12 +0000

    Updated with OrderedDict

--- LRU-cache/solution.py
+++ LRU-cache/solution.py
@@ -5,28 +5,24 @@ class LRUCache:
     def __init__(self, capacity: int):
         # hashmap of {key: value}
         self.capacity = capacity
-        self.queue = deque()
-        self.hashmap = {}
+        self.hashmap = OrderedDict()
 
     def get(self, key: int) -> int: # needs to update recently used
         if key not in self.hashmap:
             return -1
         # move key to most recent
-        self.queue.remove(key)
-        self.queue.append(key)
-        return self.hashmap.get(key, -1)
+        self.hashmap.move_to_end(key)
+        return self.hashmap[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self.hashmap:
-            self.queue.remove(key)
-
-        elif len(self.queue) == self.capacity:
-            lru = self.queue.popleft()
-            del self.hashmap[lru]
-
+            # then i need to move it back to the end
+            self.hashmap.move_to_end(key)
         self.hashmap[key] = value
-        self.queue.append(key)
 
+        if len(self.hashmap) > self.capacity:
+            self.hashmap.popitem(last = False)
+        
         
         
 
