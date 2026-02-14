1aa8a4e5fabcf161041374a0d0499b8c3041cbdb
Ellis <minibroel@gmail.com>
2026-02-14 20:28:44 +0000

    Depth first recursion

--- invert-a-binary-tree/solution.py
+++ invert-a-binary-tree/solution.py
@@ -9,13 +9,10 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
-        queue = deque([root])
-        while queue:
-            node = deque.popleft(queue)
-            if node.left:
-                queue.append(node.left)
-                
-            if node.right:
-                queue.append(node.right)
-            node.left, node.right = node.right, node.left
+       
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
         return root
