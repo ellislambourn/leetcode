33999e3cf5b259f7b3788ee5f677e3697ed215ee
Ellis <minibroel@gmail.com>
2026-02-15 16:01:37 +0000

    iterative depth first

--- maximum-depth-of-binary-tree/solution.py
+++ maximum-depth-of-binary-tree/solution.py
@@ -7,24 +7,18 @@
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        #  bfs approach
+        #  iterative df approach
         if not root:
-            return 0 
-
-        q = deque([root])
+            return 0
         res = 1
-
-        while q:
-
-            for i in range(len(q)):
-
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                
-                if node.right:
-                    q.append(node.right)
+        stack = [(root, 1)] # (node, depth)
+        while stack:
+            node, depth = stack.pop()
+            if node.right:
+                stack.append((node.right, depth+1))
             
-            res +=1
-
-        return res -1
+            if node.left:
+                stack.append((node.left, depth+1))
+            res = max(res, depth)
+        return res
+            
