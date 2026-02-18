d6d9b53e0e971166b596f2549de4547d0a2d904e
Ellis <minibroel@gmail.com>
2026-02-18 16:49:45 +0000

    iterative dfs with one stack

--- same-tree/solution.py
+++ same-tree/solution.py
@@ -5,36 +5,20 @@
 #         self.left = left
 #         self.right = right
 
-from collections import deque
-
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # bfs twice
-        if p is None and q is None:
-            return True
-        elif p is None or q is None:
-            return False
-        queue1 = deque([p])
-        queue2 = deque([q])
+        # iterative dfs with one stack
+        stack = [(p,q)]
         
-        while queue1 or queue2:
-            if not (queue1 and queue2):
-                return False # test if there are more nodes in one tree
-            node1, node2 = queue1.popleft(), queue2.popleft()
-            if not (node1.val == node2.val):
-                return False
-            if bool(node1.right) != bool(node2.right):
-                return False
-            if bool(node1.left) != bool(node2.left):
-                return False
-            if node1.left:
-                queue1.append(node1.left)
-            if node2.left:
-                queue2.append(node2.left)
-            if node1.right:
-                queue1.append(node1.right)
-            if node2.right:
-                queue2.append(node2.right)
+        while stack:
+            nodes = stack.pop()
+            if not any(nodes):
+                continue
+            if not all(nodes) or nodes[0].val != nodes[1].val:
+                return False # checks if only one is None as well as chcks theyre values once established they are nodes.
+
+            stack.append((nodes[0].left, nodes[1].left))
+            stack.append((nodes[0].right, nodes[1].right))
+        
         else:
             return True
-            
