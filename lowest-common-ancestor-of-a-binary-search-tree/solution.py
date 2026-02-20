3dd0713f50b573d4446285d9e4505f9b3a98cb3d
Ellis <minibroel@gmail.com>
2026-02-20 14:09:40 +0000

    compare through first traversal

--- lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -7,41 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        # make p < q. the ancestor a is in p < a < q so brute force would be to check every node wich would be nlogn. 
-        # or i just traverse to p and then to q and keep track of nodes visited to reach them.
-
-        # traverse to p.
-        pNodes = [root]
+        # compare as we traverse.
         curr = root
-
-        while curr.val != p.val:
-            if p.val > curr.val:
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
                 curr = curr.right
-            else:
+            elif p.val < curr.val and q.val < curr.val:
                 curr = curr.left
-            pNodes.append(curr)
-        
-        # traverse to q.
-        qNodes = [root]
-        curr = root
-
-        while curr.val != q.val:
-            if q.val > curr.val:
-                curr = curr.right
             else:
-                curr = curr.left
-            qNodes.append(curr)
-
-        # compare ancestrees (lol)
-
-        res = root
-        length = min(len(pNodes), len(qNodes))
-        for i in range(length):
-            if pNodes[i] is qNodes[i]:
-                res = pNodes[i] 
-            else:
-                break
-        
-        return res
-        
-    
+                return curr
+        # only difference in algorithms is this one traverse once whereas the last one traverses three times.
