4ae7f287e6ea2cb3c9402dc61dca29bdb80f4e3d
Ellis <minibroel@gmail.com>
2026-03-12 15:36:08 +0000

    sorted backtracking

--- combination-sum/solution.py
+++ combination-sum/solution.py
@@ -1,23 +1,21 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        #  sorted version so i can optimise and not checki larger numbers when total > target.
         res = []
         subset = []
+        nums.sort()
+
         def dfs(i, total):
-            if i >= len(nums) or total > target:
-                return
             if total == target:
                 res.append(subset.copy())
                 return
             
-            # include i
-            subset.append(nums[i])
-            total += nums[i]
-            dfs(i, total)
-            
-            # dont include i
+            for j in range(i, len(nums)):
+                if total + nums[j] > target: # the rest of the list wont work either
+                    return
+                subset.append(nums[j])
+                dfs(j, total + nums[j])
+                subset.pop()
 
-            subset.pop()
-            total -= nums[i]
-            dfs(i+1, total)
         dfs(0,0)
         return res
