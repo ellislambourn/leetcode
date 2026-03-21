e795913ebc9fbf85c3c7c626a98f627ed893abd2
Ellis <minibroel@gmail.com>
2026-03-21 20:40:26 +0000

    bfs

--- number-of-islands/solution.py
+++ number-of-islands/solution.py
@@ -1,27 +1,32 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         res = 0
+        # similar to dfs, ill just use a bfs function to do mark surrounding land as water.
 
-        def dfs(rowI, colI):
+        def bfs(rowI, colI):
             nonlocal grid
-            if (rowI < 0 or 
+            q = deque([(rowI, colI)])
+            while q:
+                rowI, colI = q.popleft()
+                if (rowI < 0 or 
             rowI >= len(grid) or
             colI < 0 or 
             colI >= len(grid[0]) or 
             grid[rowI][colI] == "0"):
-                return
-            
-            grid[rowI][colI] = "0"
-            dfs(rowI+1, colI)
-            dfs(rowI-1, colI)
-            dfs(rowI, colI+1)
-            dfs(rowI, colI-1)
-             
+                    continue
+                grid[rowI][colI] = "0"
+
+                q.append((rowI+1, colI))
+                q.append((rowI-1, colI))
+                q.append((rowI, colI+1))
+                q.append((rowI, colI-1))
 
         for rowI in range(len(grid)):
             for colI in range(len(grid[0])):
                 if grid[rowI][colI] == "1":
-                    res += 1
-                    dfs(rowI, colI)
+                    res +=1
+                    bfs(rowI, colI)
         
         return res
