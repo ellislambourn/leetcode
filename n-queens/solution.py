24b66cd95bcf3dd21684e5a67d9ed6aac811a782
Ellis <minibroel@gmail.com>
2026-03-21 00:28:28 +0000

    backtracking + check

--- n-queens/solution.py
+++ n-queens/solution.py
@@ -1,18 +1,36 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
-        board = ["." * n] * n
+        self.board = ["." * n for _ in range(n)]
 
         def rec(row):
+
             if row >= n:
-                res.append(board.copy())
+                res.append(self.board.copy())
                 return
             
-            for col in range(n):
-                if check():
-                    board[row][col] = "." * + "Q" + "." *
-                    rec(row+1)
-                    board[row][col] = "." * n
+            for col in self.check(row, n):
+                self.board[row] = "." * col + "Q" + "." * (n-col-1)
+                rec(row+1)
+                self.board[row] = "." * n
+            
+        rec(0)
 
+        return res
 
-    def check(self, coords):
+    def check(self, row, n) -> list[int]:
+        if row == 0:
+            return list(range(n))
+        banned = set()
+        for rIndex in range(row):
+            queenI = self.board[rIndex].index("Q")
+            banned.add(queenI)
+            left = -(row - rIndex)
+            right = -left
+            if queenI + left >=0:
+                banned.add(queenI+left)
+            if queenI + right < n:
+                banned.add(queenI + right)
+            
+        
+        return list(set(range(n)) - banned)
